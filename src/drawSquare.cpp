b514e93e5b05ecac791e5e78cfe046b75210da86
TommasoBarbiero9 <tommasobarbiero9@gmail.com>
2025-07-26 19:51:43 +0200

    Added libs, created first square drawing program

---
+++ src/drawSquare.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include "SDL2/SDL.h"
+#include "GL/glew.h"
+
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#endif
+
+SDL_Window *window;
+SDL_GLContext gl_context;
+
+bool is_game_running;
+
+void set_sdl_gl_attributes()
+{
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+}
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\0";
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+    {
+        std::cout << "SDL2 initialized successfully." << std::endl;
+        set_sdl_gl_attributes();
+
+        window = SDL_CreateWindow("Game", 0, 0, 1024, 768, SDL_WINDOW_OPENGL);
+        gl_context = SDL_GL_CreateContext(window);
+
+        if (glewInit() == GLEW_OK)
+        {
+            std::cout << "GLEW initialization successful" << std::endl;
+        }
+        else
+        {
+            std::cout << "GLEW initialization failed" << std::endl;
+            return -1;
+        }
+
+        unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+
+        unsigned int fragmentShader;
+        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+
+        unsigned int shaderProgram;
+        shaderProgram = glCreateProgram();
+
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        /*
+                == Defined 6 vertices to draw two different triangles ==
+                float vertices[] = {
+                    -0.7f, -0.5f, 0.0f,
+                    -0.2f, -0.5f, 0.0f,
+                    -0.45f, 0.5f, 0.0f,
+                    0.2f, -0.5f, 0.0f,
+                    0.7f, -0.5f, 0.0f,
+                    0.45f, 0.5f, 0.0f};
+        */
+
+        float vertices[] = {
+            0.5f, 0.5f, 0.0f,   // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f, // bottom left
+            -0.5f, 0.5f, 0.0f   // top left
+        };
+
+        unsigned int indices[] = {
+            // note that we start from 0!
+            0, 1, 3, // first Triangle
+            1, 2, 3  // second Triangle
+        };
+
+        unsigned int VBO, VAO, EBO;
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+        glBindVertexArray(VAO);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+
+        SDL_Event event;
+        is_game_running = true;
+
+        while (is_game_running)
+        {
+            while (SDL_PollEvent(&event))
+            {
+                if (event.type == SDL_KEYDOWN)
+                {
+                    if (event.key.keysym.sym == SDLK_ESCAPE)
+                    {
+                        is_game_running = false;
+                    }
+                }
+
+                if (event.type == SDL_QUIT)
+                {
+                    is_game_running = false;
+                }
+            }
+
+            glClearColor(0, 0, 0, 1);
+            glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+            glUseProgram(shaderProgram);
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+            SDL_GL_SwapWindow(window);
+        }
+
+        glDeleteVertexArrays(1, &VAO);
+        glDeleteBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
+        glDeleteProgram(shaderProgram);
+        SDL_Quit();
+    }
+    else
+    {
+        std::cout << "SDL2 initialization failed." << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
